61d3237227454473a9c01f56fc4346abd491bfb0
Riley Carpenter <31159790+rileyball@users.noreply.github.com>
2019-01-28 06:35:42 -0500

    Automatically update .desktop permission
    It doesn't give it the correct permissions yet but I'm working on it. The code is there for it too give the permissions though

--- DesktopFileCreator.java
+++ DesktopFileCreator.java
@@ -1,6 +1,7 @@
-/*
+package com.company;/*
  Title: This creates a .desktop file in java
  Author: Riley Carpenter
+ TODO: Make the program able to move the .desktop file to ~/.local/share/applications
  */
 
 
@@ -9,41 +10,45 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 
-public class DesktopFileCreator {
-	
-	
-	//Creates the File
-	public static void createDesktopFile(String fileName, String exe, String ico) throws IOException {
-		List<String> lines = Arrays.asList("[Desktop Entry]", "Encoding=UTF-8","Version=1.0","Type=Application","Terminal=false","Exec=" +exe,"Name=" +fileName,"Icon=" + ico);
-		Path file = Paths.get(System.getProperty( "user.home" ) + "/.local/share/applications/" + fileName + ".desktop");
-
-		//Sets File's Permissions
-		Set<PosixFilePermission> ownerWritable = PosixFilePermissions.fromString("rw-r--r--");
-		FileAttribute<?> permissions = PosixFilePermissions.asFileAttribute(ownerWritable);
+public class Main {
 
 
-		Files.write(file, permissions, lines, Charset.forName("UTF-8"));
-	}
+    //Creates the File
+    public static void createDesktopFile(String fileName, String exe, String ico) throws IOException {
+        List<String> lines = Arrays.asList("[Desktop Entry]", "Encoding=UTF-8","Version=1.0","Type=Application","Terminal=false","Exec=" +exe,"Name=" +fileName,"Icon=" + ico);
+        Path file = Paths.get(System.getProperty( "user.home" ) + "/.local/share/applications/" + fileName + ".desktop");
 
-	
-	//Main method
-	public static void main(String[] args) throws IOException{
-		Scanner reader = new Scanner(System.in);
-		System.out.print("What is the name of the executable? ");
-		String fileName = reader.nextLine();
-		System.out.print("What is the file path for the executable? ");
-		String executableFile = reader.nextLine();
-		System.out.print("What is the icon path for the file? (Leave blank if none) ");
-		String iconPath = reader.nextLine();
-		reader.close();
-		createDesktopFile(fileName, executableFile, iconPath);
-		System.out.print("\u001b[2J");
-		System.out.flush();
-		System.out.println("Desktop File Created Successfully!");
-	}
+        //Sets File's Permissions
+        Set<PosixFilePermission> ownerWritable = PosixFilePermissions.fromString("rw-r--r--");
+        FileAttribute<?> permissions = PosixFilePermissions.asFileAttribute(ownerWritable);
+
+        Files.createFile(file, permissions);
+        Files.write(file, lines, Charset.forName("UTF-8"));
+    }
+
+
+    //Main method
+    public static void main(String[] args) throws IOException{
+        Scanner reader = new Scanner(System.in);
+        System.out.print("What is the name of the executable? ");
+        String fileName = reader.nextLine();
+        System.out.print("What is the file path for the executable? ");
+        String executableFile = reader.nextLine();
+        System.out.print("What is the icon path for the file? (Leave blank if none) ");
+        String iconPath = reader.nextLine();
+        reader.close();
+        createDesktopFile(fileName, executableFile, iconPath);
+        System.out.print("\u001b[2J");
+        System.out.flush();
+        System.out.println("Desktop File Created Successfully!");
+    }
 }
